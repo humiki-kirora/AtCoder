3c2756fe7bd30d4f275f3d581474c683e7712252
humiki-kiroa <h.kimura.github@gmail.com>
2023-03-29 01:39:29 +0900

    problem solve (progress) 2023/03/29

---
+++ EX1-コードテストと出力の練習/main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+int main(){
+    std::cout << "こんにちは" << std::endl;
+    std::cout << "AtCoder" << std::endl;
+
+    return 0;
+}
---
+++ EX3-計算問題/main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  cout << 1.0f / 2.0f * 100 * (100 + 1) << endl;
+
+  return 0;
+}
