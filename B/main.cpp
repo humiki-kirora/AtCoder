ef6fdc78ddade915ce63b2ad14564db646ae9f93
humiki-kiroa <h.kimura.github@gmail.com>
2023-12-04 00:50:44 +0900

    template update

--- B/main.cpp
+++ B/main.cpp
@@ -1,12 +1,93 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ULLong unsigned long long
-#define ULong unsigned long
-#define LLong long long
 
-int N,M,Q,K,T;
+/***type templete***/
+#define ll long long
+#define u32 unsigned int
+#define u64 unsigned long long
+#define d64 double
+#define pii pair<int,int>
+#define pci pair<char,int>
+
+/***vector templete ***/
+typedef vector<int> vi;
+#define INTI_VI(N) vi(N,0)
+
+typedef vector<u64> vu64;
+#define INTI_VU64(N) vu64(N,0)
+
+typedef vector<string> vs;
+#define INTI_VS(N) vs(N)
+
+typedef vector<vi> vvi;
+#define INTI_VVI(N) vvi(N,vi(N,0))
+
+typedef vector<vu64> vvu64;
+#define INTI_VVU64(N) vvu64(N,vu64(N,0))
+
+
+/***input template***/
+template<class T>
+void read_v(vector<T> & args){
+    for(int i = 0; i < args.size(); i ++) cin >> args[i];
+}
+
+template<class T>
+void read_vv(vector<vector<T>> & args){
+    for(int i = 0; i < args.size(); i ++)
+        for(int j = 0; j < args.size(); j ++) cin >> args[i][j];
+}
+
+/***output templete***/
+template<class T>
+void ANSWER(T ans, bool end = true){
+    if(end) cout << ans << endl;
+    else cout << ans << " ";
+}
+
+template<class T>
+void ANSWER_ARRAY(vector<T> ans){
+    for(int i = 0; i < ans.size() - 1; i ++){
+        ANSWER(ans[i],false);
+    }
+    ANSWER(ans.back());
+}
+
+template<class T>
+void ANSWER_ARRAYS(vector<vector<T>> ans){
+    for(int i = 0; i < ans.size(); i ++){
+        for(int j = 0; j < ans.size() - 1; j ++){
+            ANSWER(ans[i][j],false);
+        }
+        ANSWER(ans[i].back());
+    }
+}
+
+void YESorNO(bool ans){
+    if(ans) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
+
+void solve(){
+    u64 N,M,K,Q;
+    cin >> N;
+
+    auto A = INTI_VU64(N);
+    read_v(A);
+
+    auto B = INTI_VVU64(N);
+    read_vv(B);
+
+    auto S = INTI_VS(N);
+    read_v(S);
+
+    ANSWER_ARRAY(A);
+    ANSWER_ARRAYS(B);
+    ANSWER_ARRAY(S);
+    YESorNO(true);
+}
 
 int main(){
-    cin >> N;
+    solve();
     return 0;
 }
